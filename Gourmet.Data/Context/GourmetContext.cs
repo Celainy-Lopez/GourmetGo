c274d15a44fb6518ac8fe6fbab77b4bae6515e5d
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-11-25 15:03:19 -0400

    Creando el contexto

--- Gourmet.Data/Context/GourmetContext.cs
+++ Gourmet.Data/Context/GourmetContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Gourmet.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +8,11 @@ using System.Threading.Tasks;
 
 namespace Gourmet.Data.Context;
 
-internal class GourmetContext
+public class GourmetContext(DbContextOptions<GourmetContext> options) : DbContext(options)
 {
+	public DbSet<Carritos> Carritos { get; set; }
+	public DbSet<MetodosPago> MetodosPagos { get; set; }
+	public DbSet<Ordenes> Ordenes { get; set; }
+	public DbSet<Productos> Productos { get; set; }
+	public DbSet<Usuarios> Usuarios { get; set; }
 }
